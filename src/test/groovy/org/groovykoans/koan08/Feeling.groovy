f5f4cbb199d1882f8f561860cb3df031914158a3
Lovinder Pnag <lspnag@gmail.com>
2016-03-17 04:15:43 -0400

    more goodness

--- src/test/groovy/org/groovykoans/koan08/Feeling.groovy
+++ src/test/groovy/org/groovykoans/koan08/Feeling.groovy
@@ -11,8 +11,13 @@ public enum Feeling {
     Happy, Sad, Neutral, Suicidal, Anticipation, Surprised, Relaxed, Guilty
 
     // ------------ START EDITING HERE ----------------------
+    boolean isCase(Cartoon cartoon) {
+        cartoon.feeling == this
+    }
 
-
+    boolean isCase(Person person) {
+        person.feelings.contains(this)
+    }
     // ------------ STOP EDITING HERE  ----------------------
 
 
